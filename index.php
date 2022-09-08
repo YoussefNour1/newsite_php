05a8d00a2de92765c8afd90fd29c7833692ecdf2
Youssef Nour <81862730+YoussefNour1@users.noreply.github.com>
2022-09-09 01:47:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 
-echo "<h1 style='color: red; font-size: 50px'>Fuck you Yassen</h1>";
+echo "<h1 style='color: red; font-size: 50px'>Hello!</h1>";
+echo "<p>this is an example on deploying a website on server</p>";
